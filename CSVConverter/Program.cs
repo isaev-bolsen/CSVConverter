8bd5e071f574c4180c897ac3cbc18204ce04db8d
isaev-bolsen <al_mamistov@mail.ru>
2017-03-10 20:23:27 +0300

    Parse args MK1

--- CSVConverter/Program.cs
+++ CSVConverter/Program.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OfficeOpenXml.Core.ExcelPackage;
+using System.IO;
 
 namespace CSVConverter
 {
     class Program
     {
+        private static List<FileInfo> _filesToProcess = new List<FileInfo>();
+
         static void Main(string[] args)
         {
+            foreach (string path in args) ParsePath(path);
+        }
+
+        private static void ParsePath(string path)
+        {
+            try
+            {
+                FileInfo FileInfo = new FileInfo(path);
+                if (FileInfo.Exists)
+                {
+                    _filesToProcess.Add(FileInfo);
+                    Console.WriteLine("File found: " + FileInfo.FullName);
+                    return;
+                }
+            }
+            catch { }
+            Console.WriteLine("File NOT found: " + path);
         }
     }
 }
