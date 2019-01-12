41e2cd5b8c95a52b9ab70101fff53c19353e8eb7
isaev-bolsen <al_mamistov@mail.ru>
2019-01-12 14:26:39 +0300

    Extract ConvertFile

--- CSVConverter/CSVConverter.cs
+++ CSVConverter/CSVConverter.cs
@@ -12,16 +12,21 @@ namespace CSVConverter
             foreach (FileInfo file in files)
                 try
                 {
-                    string Filename = Path.GetFileNameWithoutExtension(file.Name);
-                    ExcelWorksheet[] worksheets = new ExcelPackage(file).Workbook.Worksheets.Cast<ExcelWorksheet>().ToArray();
-                    if (worksheets.Count() == 1) new Page(worksheets.Single()).Save(GetResultPath(file.Directory, Filename));
-                    else foreach (ExcelWorksheet ws in worksheets)
-                            new Page(ws).Save(GetResultPath(file.Directory, string.Join("_", Filename, ws.Name)));
+                    ConvertFile(file);
                 }
                 catch (Exception exc)
                 {
                     Console.Error.WriteLine(string.Concat(file.FullName, " - ", exc.Message));
                 }
+        }
+
+        private static void ConvertFile(FileInfo file)
+        {
+            string Filename = Path.GetFileNameWithoutExtension(file.Name);
+            ExcelWorksheet[] worksheets = new ExcelPackage(file).Workbook.Worksheets.Cast<ExcelWorksheet>().ToArray();
+            if (worksheets.Count() == 1) new Page(worksheets.Single()).Save(GetResultPath(file.Directory, Filename));
+            else foreach (ExcelWorksheet ws in worksheets)
+                    new Page(ws).Save(GetResultPath(file.Directory, string.Join("_", Filename, ws.Name)));
         }
 
         private static FileInfo GetResultPath(DirectoryInfo directory, string fileName)
