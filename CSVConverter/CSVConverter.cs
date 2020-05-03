04d3fd27517e4f7079a4b6b1418d233b557581af
Alexandr Mamistov <amamistov@acumatica.com>
2020-05-03 09:19:19 +0300

    return resulting files

--- CSVConverter/CSVConverter.cs
+++ CSVConverter/CSVConverter.cs
@@ -1,5 +1,5 @@
 ﻿using OfficeOpenXml;
-using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -7,26 +7,18 @@ namespace CSVConverter
 {
     public static class CSVConverter
     {
-        public static void Convert(params FileInfo[] files)
+        public static IEnumerable<FileInfo> Convert(params FileInfo[] files)
         {
-            foreach (FileInfo file in files)
-                try
-                {
-                    ConvertFile(file);
-                }
-                catch (Exception exc)
-                {
-                    Console.Error.WriteLine(string.Concat(file.FullName, " - ", exc.Message));
-                }
+            return files.SelectMany(ConvertFile).ToArray();
         }
 
-        private static void ConvertFile(FileInfo file)
+        private static IEnumerable<FileInfo> ConvertFile(FileInfo file)
         {
             string Filename = Path.GetFileNameWithoutExtension(file.Name);
             ExcelWorksheet[] worksheets = new ExcelPackage(file).Workbook.Worksheets.Cast<ExcelWorksheet>().ToArray();
-            if (worksheets.Count() == 1) new Page(worksheets.Single()).Save(GetResultPath(file.Directory, Filename));
+            if (worksheets.Count() == 1) yield return new Page(worksheets.Single()).Save(GetResultPath(file.Directory, Filename));
             else foreach (ExcelWorksheet ws in worksheets)
-                    new Page(ws).Save(GetResultPath(file.Directory, string.Join("_", Filename, ws.Name)));
+                    yield return new Page(ws).Save(GetResultPath(file.Directory, string.Join("_", Filename, ws.Name)));
         }
 
         private static FileInfo GetResultPath(DirectoryInfo directory, string fileName)
